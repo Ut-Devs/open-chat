6d3c9988356c3a2ee809ed764377eafe1641098a
jowjow22 <jonathan224santos@gmail.com>
2023-02-02 15:28:52 -0300

    fix: change import from vue-router

--- src/Views/Private/Chat.vue
+++ src/Views/Private/Chat.vue
@@ -1,11 +1,5 @@
 <template>
+  <div>
     <h1>Chat</h1>
+  </div>
 </template>
-
-<script lang="ts">
-    import { defineComponent } from 'vue';
-    
-    export default defineComponent({
-        name: 'Chat'
-    })
-</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,8 @@
-import Router from 'vue-router'
-import { CHAT } from '../router/Private/Chat'
+import { createRouter, createWebHistory } from "vue-router";
+import { CHAT } from "../router/Private/Chat";
 
-const Home = { template: '<h1>Bom dia</h1>' }
-
-const routes = [
-    { path: '/', component: Home },
-  ]
-
-const router = Router.createRouter({
-    history: Router.createWebHistory(),
-    routes
-})
-export default router
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [CHAT],
+});
+export default router;
