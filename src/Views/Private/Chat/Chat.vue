8c2afba37f3821b53c4a76b3ca9e8a95f1d87367
jowjow22 <jonathan224santos@gmail.com>
2023-02-02 17:13:34 -0300

    feat: create base desing and add new css styles

--- src/Views/Private/Chat/Chat.vue
+++ src/Views/Private/Chat/Chat.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="chat">
-    <nav class="chat__header">
-       
-    </nav>
-    <section class="chat__body">
-        
-    </section>
+    <nav class="chat__header"></nav>
+    <section class="chat__body"></section>
     <div class="chat__send-message">
-        
+      <input type="text" class="chat__send-message__input" />
+      <button class="chat__send-message__button">Send</button>
     </div>
   </div>
 </template>
