3ef98dbfe6afb5f9c49edb3ea1399edf56c40729
DiogoKaster <diogokaster@gmail.com>
2023-02-09 14:07:33 -0300

    feat: correct messages box style and add when the message was sent

--- src/Views/Private/Chat/components/Messages/Messages.vue
+++ src/Views/Private/Chat/components/Messages/Messages.vue
@@ -5,13 +5,22 @@
 			{ 'message__wrapper--recipient': !isSender },
 		]"
 	>
-		<span>{{ message.content }}</span>
+		<div
+			:class="[
+				{ 'message__wrapper--sender-content': isSender },
+				{ 'message__wrapper--recipient-content': !isSender },
+			]"
+		>
+			<span>{{ message.content }}</span>
+		</div>
+		<span>{{ date }}</span>
 	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import { IMessage } from '@/models/Message.model'
+import { getFormatedDate } from '@/helpers/date'
 
 export default defineComponent({
 	name: 'Messages',
@@ -28,6 +37,9 @@ export default defineComponent({
 			} else {
 				return false
 			}
+		},
+		date() {
+			return getFormatedDate(this.message.sendAt)
 		},
 	},
 })
