c5e3985164868ca0e6536f424d34d230632b49e3
jowjow22 <jonathan224santos@gmail.com>
2023-02-19 20:48:04 -0300

    feat: add dist folder

---
+++ dist/assets/Home-34ea4adf.js
@@ -0,0 +1,25 @@
+import {
+	_ as o,
+	c as t,
+	e as _,
+	w as s,
+	g as c,
+	o as a,
+	h as n,
+} from './index-318c2000.js'
+const r = {},
+	d = { class: 'home' }
+function l(f, m) {
+	const e = c('router-link')
+	return (
+		a(),
+		t('main', d, [
+			_(e, { to: '/chat' }, { default: s(() => [n('Go to Chat')]), _: 1 }),
+		])
+	)
+}
+const p = o(r, [
+	['render', l],
+	['__scopeId', 'data-v-ddbef906'],
+])
+export { p as default }
