81b7c0ee31f75ff18b0c2215fce072e0097dcd15
jowjow22 <jonathan224santos@gmail.com>
2023-02-19 20:55:12 -0300

    ci: add deployment file to project

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue(), eslintPlugin()],
+	base: '/open-chat/',
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
