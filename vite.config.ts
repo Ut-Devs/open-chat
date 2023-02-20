6b0f1248bd4de8674176922a894917aed625f035
jowjow22 <jonathan224santos@gmail.com>
2023-02-19 21:22:44 -0300

    feat: change vite path

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue(), eslintPlugin()],
-	base: '/open-chat/',
+	base: './',
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
