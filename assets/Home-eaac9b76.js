4a1c3f7721e8af6eb3469e45a69fdef9c1d23653
jowjow22 <jowjow22@users.noreply.github.com>
2023-02-20 00:40:46 +0000

    deploy: e1477b4c179dc955a1d97c7774a04112b7e32f87

--- assets/Home-eaac9b76.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as t,e as _,w as s,g as c,o as a,h as n}from"./index-f2892886.js";const r={},d={class:"home"};function l(f,m){const e=c("router-link");return a(),t("main",d,[_(e,{to:"/chat"},{default:s(()=>[n("Go to Chat")]),_:1})])}const p=o(r,[["render",l],["__scopeId","data-v-ddbef906"]]);export{p as default};
