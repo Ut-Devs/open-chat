70b3e8445c5826946534ca1a055d2aef2b41c1d0
jowjow22 <jonathan224santos@gmail.com>
2023-02-19 22:34:12 -0300

    test: try to fix vitest

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,6 @@ import '@assets/css/globals.scss'
 import App from './App.vue'
 import router from '@router/index'
 
+alert(process.env.NODE_ENV)
+
 createApp(App).use(router).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		vue({
